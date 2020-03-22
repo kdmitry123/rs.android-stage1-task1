cd80b9a0f7dd3ec99244f558fdb8cee941c43cd9
karachun.dmitry@mail.ru <1282s8u6>
2020-03-22 08:19:58 +0300

    refactor: change var to val

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,9 +4,9 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        var sum = input.sum()
-        var max = input.max()
-        var min = input.min()
+        val sum = input.sum()
+        val max = input.max()
+        val min = input.min()
         return arrayOf(sum - max!!, sum - min!!).toIntArray()
 
     }
