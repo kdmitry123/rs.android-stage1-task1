07141f0a2d70aeb2c3a4bd520e7628be27df3297
karachun.dmitry@mail.ru <1282s8u6>
2020-03-22 08:19:01 +0300

    feat: add solution task_2

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,10 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sum = input.sum()
+        var max = input.max()
+        var min = input.min()
+        return arrayOf(sum - max!!, sum - min!!).toIntArray()
+
     }
 }
