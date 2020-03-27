2f2277cd16c47dcbaa5a8672b356603a0abf5c79
karachun.dmitry@mail.ru <1282s8u6>
2020-03-27 13:01:16 +0300

    feat: add solution task_5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,22 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var counter = 0
+        var max = digitString.max()
+        var sb = StringBuilder(digitString)
+        for (i in 0 until digitString.length / 2) {
+            if (digitString[i] != digitString[digitString.length - i - 1]) {
+                if (max != null) {
+                    sb[i] = max
+                    sb[digitString.length - i - 1] = max
+                    counter++
+                }
+            }
+            if (counter > k) {
+                return (-1).toString()
+            }
+        }
+        return sb.toString()
     }
 }
